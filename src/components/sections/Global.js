9a011b625f530a9b215e46d8b23e51575c7f0593
Adam <adam.parr12@gmail.com>
2020-03-30 14:36:43 +0100

    display global stats

--- src/components/sections/Global.js
+++ src/components/sections/Global.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Card from '@material-ui/core/Card'
 
 export default function Global({data, timeline}) {
 
@@ -11,25 +13,40 @@ export default function Global({data, timeline}) {
   const { cases: prevCases, deaths: prevDeaths, recovered: prevRecovered } = timeline[yesterday];
 
   return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={4}
-    >
-      <div>
-        <p>cases: {cases}</p>
-        <p>{cases - prevCases} today</p>
-      </div>
-      <div>
-        <p>deaths: {deaths}</p>
-        <p>{deaths - prevDeaths} today</p>
-      </div>
-      <div>
-        <p>recovered: {recovered}</p>
-        <p>{recovered - prevRecovered} today</p>
-      </div>
-    </Grid>
+    <Container className="globalStats">
+      <Grid
+        container
+        spacing={3}
+        justify="center"
+      >
+        <Grid xs={12} sm item>
+          <Card className="card">
+            <h3>Cases</h3>
+            <span className="stat">{cases}</span>
+            <span className="prevStat">{cases - prevCases} today</span>
+            <div className="bar blue"/>
+          </Card>
+        </Grid>
+
+        <Grid xs item>
+          <Card className="card">
+            <h3>Deaths</h3>
+            <span className="stat">{deaths}</span>
+            <span className="prevStat">{deaths - prevDeaths} today</span>
+            <div className="bar red"/>
+          </Card>
+        </Grid>
+
+        <Grid xs item>
+          <Card className="card">
+            <h3>Recovered</h3>
+            <span className="stat">{recovered}</span>
+            <span className="prevStat">{recovered - prevRecovered} today</span>
+            <div className="bar green"/>
+          </Card>
+        </Grid>
+
+      </Grid>
+    </Container>
   )
 }
