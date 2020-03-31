50b5afd1293311d9f34f23d131f3d0696e789ddc
Adam <adam.parr12@gmail.com>
2020-03-31 10:40:43 +0100

    Updates

---
+++ precache-manifest.e781913653c7213cded9a31468cafe94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372b69b54b8cd5ca441b2d7ee33d10b7",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "514d1376fc3982461c77",
+    "url": "/coronavirus-tracker/static/css/main.4d3be0ab.chunk.css"
+  },
+  {
+    "revision": "d695fe398499acd1b871",
+    "url": "/coronavirus-tracker/static/js/2.6cfc6eb3.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/coronavirus-tracker/static/js/2.6cfc6eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514d1376fc3982461c77",
+    "url": "/coronavirus-tracker/static/js/main.065527e8.chunk.js"
+  },
+  {
+    "revision": "44bec09273fd7d3b689e",
+    "url": "/coronavirus-tracker/static/js/runtime-main.0f2eaf3c.js"
+  }
+]);
