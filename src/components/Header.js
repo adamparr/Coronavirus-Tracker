d369757a0bf5bf5ec49aa5b4f89f3903d4423873
Adam <adam.parr12@gmail.com>
2020-03-31 16:40:13 +0100

    lasted updated functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import dateFormat from 'dateformat'
 
-export default function Header() {
+export default function Header({lastUpdated}) {
+
+  const now = new Date();
+  lastUpdated = now - lastUpdated;
   return (
     <header>
       <h1>Coronavirus Tracker</h1> 
+      <p>Last updated {dateFormat(lastUpdated, "M")} minutes ago</p>
     </header>
   )
 }
