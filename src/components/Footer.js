5a09ae1e4531f31897efa2ca93c274c4bf7507aa
Adam <adam.parr12@gmail.com>
2020-04-02 12:54:04 +0100

    bookmark & share

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
+      <p>Make sure to bookmark and share this page!</p>
       <p>Made by <a href="https://aparr.dev/" target="_blank" rel="noopener noreferrer">Adam Parr</a> - <a href="https://github.com/adamparr/coronavirus-tracker" rel="noopener noreferrer" target="_blank">GitHub</a></p>
     </footer>
   )
