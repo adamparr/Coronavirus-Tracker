d50f8b1e449b21a24fc47972bdeaea4f3854f757
Adam <adam.parr12@gmail.com>
2020-04-08 09:11:42 +0100

    text amend

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Header({lastUpdated}) {
 
+  if (lastUpdated = 'a few seconds ago') lastUpdated = 'less than a minute ago';
+
   return (
     <header>
       <h1>Coronavirus Tracker</h1> 
